aed007935f335ed062251969eddd5a3324b32231
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-12 17:31:59 -0500

    added user resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    # def edit ### can add editing functionality later
+
+    # end
+    
+    # def update
+
+    # end
+
+end
